57bab528b109992bebfd6dba7cac401b4368c78a
github username <github email address>
2018-02-23 17:52:13 -0500

    artist,doctor,patient and song,genre,artist

---
+++ lib/genre.rb
@@ -0,0 +1,21 @@
+
+require 'pry'
+
+class Genre
+
+  attr_accessor :songs, :name
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    songs << song
+  end
+
+  def artists
+    songs.map {|song| song.artist}
+  end
+
+end
